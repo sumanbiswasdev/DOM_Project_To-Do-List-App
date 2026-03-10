4995d98feee6050837ff1a6e58702e9973c79cbb
sumanbrival <indianvloggersuman@gmail.com>
2026-03-10 16:47:42 -0300

    new design was made

--- script.js
+++ script.js
@@ -1,50 +1,41 @@
 function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value.trim();
 
-let input = document.getElementById("taskInput");
+    if (taskText === "") {
+    alert("Please add task");
+    return;
+    }
 
-let taskText = input.value.trim();
+    let li = document.createElement("li");
 
-if (taskText === "") {
-alert("Please add task");
-return;
-}
+    li.className = "flex justify-between items-center bg-gray-100 p-2 rounded";
 
-let li = document.createElement("li");
-li.className = "flex justify-between items-center bg-gray-100 p-2 rounded";
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-
-let checkbox = document.createElement("input");
-checkbox.type = "checkbox";
-
-
-let span = document.createElement("span");
-span.textContent = taskText;
-span.className = "flex-1 ml-2";
+    let span = document.createElement("span")
+    span.textContent = taskText
+    span.className = "flex-1 ml-2";
 
 
-let deleteBtn = document.createElement("button");
-deleteBtn.textContent = "Delete";
-deleteBtn.className = "bg-red-500 text-white px-2 py-1 rounded";
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "bg-red-500 text-white px-2 py-1 rounded";
 
+    deleteBtn.onclick =  function () {
+    li.remove()
+    }
 
-deleteBtn.onclick = function () {
-li.remove();
+    li.appendChild(checkbox)
+    li.appendChild(span)
+    li.appendChild(deleteBtn)
+
+    document.getElementById("taskList").appendChild(li)
+
+    input.value = "";
+
 };
-
-
-li.appendChild(checkbox);
-li.appendChild(span);
-li.appendChild(deleteBtn);
-
-
-document.getElementById("taskList").appendChild(li);
-
-
-input.value = "";
-
-}
-
-
 
 function deleteChecked() {
 
